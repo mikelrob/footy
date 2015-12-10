1f1ca532d0500b5ae249c29ffc6ceafca5ed7053
Mike Robinson <mikelrob@users.noreply.github.com>
2015-12-10 21:12:07 +0000

    ensure pip, virtualenv is installed or exit
    also install xvfb and firefox

--- install_footy.sh
+++ install_footy.sh
@@ -1,3 +1,21 @@
+if ! [ $(which pip) ]; then
+  echo "Installing pip with sudo"
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python 
+  if ! [ $? -eq 0 ]; then
+    exit 1
+  fi
+fi
+
+if ! [ $(which virtualenv) ]; then
+  echo "Installing virtualenv with sudo"
+  sudo -H pip install virtualenv
+  if [ $? -eq 0 ]; then
+    exit 2
+  fi
+fi
+
+sudo apt-get install xvfb firefox
+
 virtualenv $(pwd)/py_env
 source $(pwd)/py_env/bin/activate
 pip install -r requirements.txt
