458c16a2ffe639cea1d72293a13bcc2e6c2efa68
Ubuntu <bitnami@ip-10-169-51-205.ec2.internal>
2016-09-15 19:05:13 +0100

    added extra times for cron

--- install_footy.sh
+++ install_footy.sh
@@ -20,5 +20,6 @@ virtualenv $(pwd)/py_env
 source $(pwd)/py_env/bin/activate
 pip install -r requirements.txt
 echo "0-5 7 * * 3 $(pwd)/run_footy.sh" > tmp
+echo "56-59 6 * * 3 $(pwd)/run_footy.sh" >> tmp
 crontab tmp
 rm tmp
