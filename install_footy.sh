4f6e877453540da0f53c889bda126b8b3fa6cea2
Mike Robinson <mikelrob@users.noreply.github.com>
2019-02-19 23:50:44 +0000

    Update install_footy.sh

--- install_footy.sh
+++ install_footy.sh
@@ -1,6 +1,7 @@
 if ! [ $(which pip) ]; then
   echo "Installing pip with sudo"
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python 
+  #curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python 
+  sudo apt install python-pip
   if ! [ $? -eq 0 ]; then
     exit 1
   fi
@@ -9,7 +10,7 @@ fi
 if ! [ $(which virtualenv) ]; then
   echo "Installing virtualenv with sudo"
   sudo -H pip install virtualenv
-  if [ $? -eq 0 ]; then
+  if ! [ $? -eq 0 ]; then
     exit 2
   fi
 fi
